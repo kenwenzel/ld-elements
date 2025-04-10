0470b0965ccf5319cb0f2d272da3769cd57d667c
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2025-04-10 17:01:57 +0200

    Support signals in ld-rdfa element.

--- src/components/ld-rdfa/ld-rdfa.ts
+++ src/components/ld-rdfa/ld-rdfa.ts
@@ -1,20 +1,28 @@
-import { LitElement, css, html, nothing } from 'lit';
+import { getScope } from '@heximal/components';
+import { HeximalElement } from '@heximal/element';
+import { css, html, nothing, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { until } from 'lit/directives/until.js';
 import { prepareTemplate, replaceExpressions } from '../../rdfa/lit-rdfa';
 import { RDFaToSparqlParser } from '../../rdfa/rdfa-sparql';
-import { getScope } from '@heximal/components';
+import { TemplateFunction } from '@heximal/templates';
 
 /**
  * Renders HTML templates with RDFa markup.
  */
 @customElement('ld-rdfa')
-export class LdRdfaElement extends LitElement {
+export class LdRdfaElement extends HeximalElement {
   @property()
   template?: string;
 
   @property()
   endpoint?: string;
+
+  styleTemplate?: TemplateResult;
+
+  litTemplate?: TemplateFunction;
+
+  query?: string;
 
   async runSparqlQuery(sparqlEndpointUrl: string, sparqlQuery: string): Promise<any[]> {
     try {
@@ -40,32 +48,46 @@ export class LdRdfaElement extends LitElement {
     }
   }
 
-  render() {
-    let style : HTMLStyleElement | undefined = undefined;
-    let templateElement : HTMLTemplateElement | undefined = undefined;
+  connectedCallback(): void {
+    super.connectedCallback();
+    let templateElement: HTMLTemplateElement | undefined = undefined;
     if (this.template) {
       templateElement = document.querySelector(this.template) as HTMLTemplateElement | undefined;
     } else {
-      style = this.querySelector('style') as HTMLStyleElement | undefined;
+      const styleElement = this.querySelector('style');
+      if (styleElement) {
+        this.styleTemplate = html`<style>${styleElement.innerText}</style>`;
+      }
       templateElement = this.querySelector('template') as HTMLTemplateElement | undefined;
     }
+    if (templateElement) {
+      const p = new RDFaToSparqlParser((templateElement as HTMLTemplateElement).content.querySelector('*:not(style)')!, window.location.href);
 
-    if (!templateElement || !this.endpoint) {
+      const newTemplate = document.createElement('template') as HTMLTemplateElement;
+      newTemplate.content.appendChild(p.getElement()!);
+
+      this.litTemplate = prepareTemplate(newTemplate);
+
+      this.query = p.getQuery();
+    }
+  }
+
+  render() {
+    if (!this.litTemplate || !this.endpoint || !this.query) {
       return nothing;
     }
 
-    const p = new RDFaToSparqlParser((templateElement as HTMLTemplateElement).content.querySelector('*:not(style)')!, window.location.href)
-    let query = p.getQuery();
-    query = replaceExpressions(query, getScope(this) || {});
+    let scope = getScope(this) || {};
+    let q = replaceExpressions(this.query!, scope);
 
-    const litTemplate = prepareTemplate(templateElement as HTMLTemplateElement);
-
-    const result = this.runSparqlQuery(this.endpoint, query).then(bindings => {
-      const model = { params: new URLSearchParams(window.location.search), bindings: bindings };
-      return litTemplate(model);
+    const result = this.runSparqlQuery(this.endpoint, q).then(bindings => {
+      const model = Object.create(scope);
+      model.params = new URLSearchParams(window.location.search);
+      model.bindings = bindings;
+      return this.litTemplate!(model);
     })
 
-    return html`${style}${until(result)}`;
+    return html`${this.styleTemplate}${until(result)}`;
   }
 
   static styles = css``
