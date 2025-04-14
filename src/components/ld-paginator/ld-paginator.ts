8238da4e8103789678377f6aa64a9d4d53216c45
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2025-04-14 11:41:02 +0200

    Fix pagination in combination with search.

--- src/components/ld-paginator/ld-paginator.ts
+++ src/components/ld-paginator/ld-paginator.ts
@@ -1,4 +1,4 @@
-import { css, html, LitElement } from 'lit';
+import { css, html, LitElement, PropertyValues } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 /**
@@ -15,6 +15,16 @@ export class LdPaginatorElement extends LitElement {
   @property({ type: Number })
   page: number = 0;
 
+  pageCount: number = 0;
+
+  protected willUpdate(_changedProperties: PropertyValues): void {
+    this.pageCount = Math.ceil((this.count || 0) / Math.max(this.pageSize, 1));
+    if (this.page > this.pageCount - 1) {
+      this.page = 0;
+      this.dispatchEvent(new Event('page-change', { composed: true, bubbles: true }));
+    }
+  }
+
   renderLink(page: number) {
     if (this.page == page) {
       return html`<span>${page + 1}</span>`;
@@ -22,12 +32,12 @@ export class LdPaginatorElement extends LitElement {
     return html`<a href="javascript:void(0)" @click=${() => {
       this.page = page;
 
-      this.dispatchEvent( new Event('page-change', {composed: true, bubbles: true}));
+      this.dispatchEvent(new Event('page-change', { composed: true, bubbles: true }));
     }}>${page + 1}</a>`
   }
 
   render() {
-    const pages = [...Array(Math.ceil((this.count || 0) / Math.max(this.pageSize, 1))).keys()];
+    const pages = [...Array(this.pageCount).keys()];
     return html`<div class="pages">
       ${pages.map(p => this.renderLink(p))}
     </div>`
