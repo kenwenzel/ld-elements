c2c62afb3bcfd4e520468fecd01d76e2513d52b2
Ken Wenzel <ken.wenzel@iwu.fraunhofer.de>
2025-04-14 22:33:30 +0200

    Add missing components.

--- src/bundle/ld-elements.ts
+++ src/bundle/ld-elements.ts
@@ -1,3 +1,5 @@
 export * from '../rdfa/lit-rdfa';
 export * from '../rdfa/rdfa';
 export * from '../rdfa/rdfa-sparql';
+export * from '../components/ld-rdfa/ld-rdfa';
+export * from '../components/ld-paginator/ld-paginator';
